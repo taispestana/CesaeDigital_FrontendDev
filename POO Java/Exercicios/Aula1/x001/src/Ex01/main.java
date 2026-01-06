5c9b3ac7196ff5cd670ffd36c43d85b410f39e43
Tais <taispestana5@gmail.com>
2026-01-06 12:40:38 +0000

    add POO java

---
+++ POO Java/Exercicios/Aula1/x001/src/Ex01/main.java
@@ -0,0 +1,11 @@
+package Ex01;
+
+public class main {
+    public static void main(String[] args) {
+
+        String name = "Taís";
+        int age = 25;
+        double hight = 1.70;
+        System.out.println("Meu nome é " + name + " e tenho " + age + " anos e meço " + hight + "m de altura.");
+    }
+}
