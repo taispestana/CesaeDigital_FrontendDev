17bb1dfba83c429b788d14eeeed0857925fd764f
Tais <taispestana5@gmail.com>
2026-01-20 12:12:44 +0000

    update java-trabalhofinal

--- Java/Trabalho Final/src/GameStart.java
+++ Java/Trabalho Final/src/GameStart.java
@@ -4,28 +4,48 @@ import java.util.Scanner;
 
 public class GameStart {
 
+    // Constantes para as colunas do ficheiro CSV
+    static final int COL_ID_VENDA = 0;
+    static final int COL_ID_CLIENTE = 1;
+    static final int COL_NOME_CLIENTE = 2;
+    static final int COL_CONTACTO = 3;
+    static final int COL_EMAIL = 4;
+    static final int COL_EDITORA = 5;
+    static final int COL_CATEGORIA = 6;
+    static final int COL_JOGO = 7;
+    static final int COL_VALOR = 8;
+    static final int TOTAL_COLUNAS = 9;
+
+    // Constantes de texto
+    static final String TIPO_ADMIN = "ADMIN";
+    static final String TIPO_CLIENTE = "CLIENTE";
+    static final String SENHA_ADMIN = "0805";
+
     public static void main(String[] args) throws FileNotFoundException {
-        // Ler o ficheiro CSV e carregar os dados para a matriz
-        String[][] tabelaVendas = carregarMatriz("src/resources/GameStart_V2(in).csv");
+
+        // Carregar os dados do ficheiro CSV
+        String[][] tabelaVendas = lerArquivoCsv("src/resources/GameStart_V2(in).csv");
         Scanner teclado = new Scanner(System.in);
         String modoUsuario;
 
         // Menu para selecionar o tipo de utilizador
         do {
             System.out.println("\n<3 <3 <3 Bem-vindo à GameStart <3 <3 <3");
-            System.out.print("Escolha e digite o tipo de Utilizador (ADMIN , CLIENTE ou SAIR): ");
+            System.out.println("Escolha o tipo de Utilizador: ");
+            System.out.println("ADMIN, CLIENTE ou SAIR");
+
             modoUsuario = teclado.next().toUpperCase();
 
-            if (modoUsuario.equals("ADMIN")) {
-                // Verificação de senha para ADMIN
+            // Verificação de senha para ADMIN
+            if (modoUsuario.equals(TIPO_ADMIN)) {
                 System.out.print("Digite sua senha: ");
                 String senhaAdmin = teclado.next();
-                if (senhaAdmin.equals("0805")) {
+                if (senhaAdmin.equals(SENHA_ADMIN)) {
                     menuAdmin(tabelaVendas, teclado);
                 } else {
                     System.out.println("Senha Incorreta! Por favor, tente novamente.");
                 }
-            } else if (modoUsuario.equals("CLIENTE")) {
+            } else if (modoUsuario.equals(TIPO_CLIENTE)) {
                 menuCliente(tabelaVendas, teclado);
             }
 
@@ -47,24 +67,25 @@ public class GameStart {
             System.out.println("0. Sair");
             System.out.print("Digite o numero correspondente à uma das opções acima: ");
             escolha = teclado.nextInt();
+            // Consumir a quebra de linha pendente para evitar erros de leitura futuros
+            teclado.nextLine();
 
-            //Laço de repetição para escolha do usuário
+            // Laço de repetição de escolha
             switch (escolha) {
                 case 1:
-                    imprimirMatriz(dados);
+                    exibirConteudoArquivo(dados);
                     break;
-                    //Opção que imprime
                 case 2:
-                    exibirTotais(dados);
+                    exibirTotalVendas(dados);
                     break;
                 case 3:
-                    calcularLucro(dados);
+                    exibirLucroTotal(dados);
                     break;
                 case 4:
-                    pesquisarCliente(dados, teclado);
+                    buscarClientePorId(dados, teclado);
                     break;
                 case 5:
-                    jogoMaisCaro(dados);
+                    exibirJogoMaisCaro(dados);
                     break;
                 case 0:
                     System.out.println("Saindo...");
@@ -76,7 +97,7 @@ public class GameStart {
     }
 
     // Carregar dados do ficheiro CSV para uma matriz
-    public static String[][] carregarMatriz(String pathArquivo) throws FileNotFoundException {
+    public static String[][] lerArquivoCsv(String pathArquivo) throws FileNotFoundException {
         File ficheiro = new File(pathArquivo);
 
         // Definir o tamanho da matriz
@@ -98,10 +119,12 @@ public class GameStart {
         String[][] matriz = new String[numLinhas - 1][9];
         Scanner scanDados = new Scanner(ficheiro);
 
+        // Pular a linha do cabeçalho
         if (scanDados.hasNextLine()) {
             scanDados.nextLine();
         }
 
+        // Preencher a matriz com os dados do ficheiro
         int idx = 0;
         while (scanDados.hasNextLine()) {
             String linha = scanDados.nextLine();
@@ -110,17 +133,21 @@ public class GameStart {
             }
 
             String[] cols = linha.split(";");
-            for (int col = 0; col < cols.length && col < 9; col++) {
-                matriz[idx][col] = cols[col];
+
+            // Segurança: Verificar se a linha tem todas as colunas necessárias
+            if (cols.length >= TOTAL_COLUNAS) {
+                for (int col = 0; col < cols.length && col < TOTAL_COLUNAS; col++) {
+                    matriz[idx][col] = cols[col];
+                }
+                idx++;
             }
-            idx++;
         }
         scanDados.close();
         return matriz;
     }
 
-    // Menu Opção 1. Exibir Conteúdo do Arquivo
-    public static void imprimirMatriz(String[][] dados) {
+    // Menu ADMIN Opção 1. Exibir Conteúdo do Arquivo
+    public static void exibirConteudoArquivo(String[][] dados) {
         for (int i = 0; i < dados.length; i++) {
             for (int j = 0; j < dados[i].length; j++) {
                 System.out.print(dados[i][j] + " | ");
@@ -129,40 +156,41 @@ public class GameStart {
         }
     }
 
-    // Menu Opção 2. Exibir Total de Vendas e Valor Acumulado
-    public static void exibirTotais(String[][] dados) {
+    // Menu ADMIN Opção 2. Exibir Total de Vendas e Valor Acumulado
+    public static void exibirTotalVendas(String[][] dados) {
         double valorAcumulado = 0;
         for (int i = 0; i < dados.length; i++) {
-            // A coluna 8 contém o valor do jogo
-            valorAcumulado += Double.parseDouble(dados[i][8]);
+            valorAcumulado += Double.parseDouble(dados[i][COL_VALOR]);
         }
         System.out.println("Total de Vendas: " + dados.length);
         System.out.println("Valor Total Acumulado: " + valorAcumulado + "€");
     }
 
-    // Menu Opção 3. Calcular lucro Total
-    public static void calcularLucro(String[][] dados) {
+    // Menu ADMIN Opção 3. Calcular lucro Total
+    public static void exibirLucroTotal(String[][] dados) {
         double somaTotal = 0;
+
+        // Soma todos os valores dos jogos
         for (int i = 0; i < dados.length; i++) {
-            somaTotal += Double.parseDouble(dados[i][8]);
+            somaTotal += Double.parseDouble(dados[i][COL_VALOR]);
         }
-        // Calcula o lucro total da loja (20%)
+        // Calcula o lucro total da loja com uma margem de 20%
         double margem = somaTotal * 0.20;
-        System.out.println("Lucro Total da Loja (20%): " + margem + "€");
+        System.out.println("Lucro Total da Loja: " + margem + "€");
     }
 
-    // Menu Opção 4. Pesquisar por cliente
-    public static void pesquisarCliente(String[][] dados, Scanner teclado) {
+    // Menu ADMIN Opção 4. Pesquisar por cliente
+    public static void buscarClientePorId(String[][] dados, Scanner teclado) {
         System.out.print("Digite o ID do Cliente desejado: ");
         String idAlvo = teclado.next();
         boolean achou = false;
 
         for (int i = 0; i < dados.length; i++) {
-            if (dados[i][1].equalsIgnoreCase(idAlvo)) {
-                System.out.println("\n<3 <3 <3 Informações do Cliente <3 <3 <3");
-                System.out.println("Nome: " + dados[i][2]);
-                System.out.println("Contacto: " + dados[i][3]);
-                System.out.println("Email: " + dados[i][4]);
+            if (dados[i][COL_ID_CLIENTE].equalsIgnoreCase(idAlvo)) {
+                System.out.println("\n<3 Informações do Cliente <3");
+                System.out.println("Nome: " + dados[i][COL_NOME_CLIENTE]);
+                System.out.println("Contacto: " + dados[i][COL_CONTACTO]);
+                System.out.println("Email: " + dados[i][COL_EMAIL]);
                 achou = true;
                 break;
             }
@@ -173,30 +201,30 @@ public class GameStart {
         }
     }
 
-    // Menu Opção 5. Exibir jogos mais caros e seu compradores
-    public static void jogoMaisCaro(String[][] dados) {
+    // Menu ADMIN Opção 5. Exibir jogos mais caros e seu compradores
+    public static void exibirJogoMaisCaro(String[][] dados) {
         double maxPreco = 0;
 
-        // Encontrar o maior valor na coluna 8
+        // Encontrar o maior valor na coluna COL_VALOR
         for (int i = 0; i < dados.length; i++) {
-            double precoJogo = Double.parseDouble(dados[i][8]);
+            double precoJogo = Double.parseDouble(dados[i][COL_VALOR]);
             if (precoJogo > maxPreco) {
                 maxPreco = precoJogo;
             }
         }
 
-        System.out.println("\n*** Jogo Mais Caro (" + maxPreco + "€) ***");
+        System.out.println("Jogo Mais Caro: " + maxPreco + "€");
 
         // Mostra os jogos e clientes que tem o valor máximo
         for (int i = 0; i < dados.length; i++) {
-            double precoJogo = Double.parseDouble(dados[i][8]);
+            double precoJogo = Double.parseDouble(dados[i][COL_VALOR]);
             if (precoJogo == maxPreco) {
-                System.out.println("Jogo: " + dados[i][7] + " | Comprador: " + dados[i][2]);
+                System.out.println("Jogo: " + dados[i][COL_JOGO] + " | Comprador: " + dados[i][COL_NOME_CLIENTE]);
             }
         }
     }
 
-    // Menu Opção 6. Menu Cliente
+    // Menu Cliente
     public static void menuCliente(String[][] dados, Scanner teclado) {
         int escolha;
         do {
@@ -212,16 +240,16 @@ public class GameStart {
 
             switch (escolha) {
                 case 1:
-                    registarNovoCliente(teclado);
+                    inserirNovoCliente(teclado);
                     break;
                 case 2:
-                    exibirVagasEstacionamento();
+                    verificarVagasDisponiveis();
                     break;
                 case 3:
-                    exibirJogosSemRepetir(dados);
+                    listarJogosUnicos(dados);
                     break;
                 case 4:
-                    pesquisarPorEditora(dados, teclado);
+                    listarJogosPorEditora(dados, teclado);
                     break;
                 case 0:
                     System.out.println("Volta para o menu principal");
@@ -232,9 +260,9 @@ public class GameStart {
         } while (escolha != 0);
     }
 
-    // Menu Opção 6.1. Registar Novo Cliente
-    public static void registarNovoCliente(Scanner teclado) {
-        System.out.println("\n> Inserir Cliente");
+    // Menu CLIENTE Opção 1. Registar Novo Cliente
+    public static void inserirNovoCliente(Scanner teclado) {
+        System.out.println("\n<3 Inserir Cliente <3");
         System.out.print("Nome: ");
         String nomeCli = teclado.nextLine();
         System.out.print("Contacto: ");
@@ -245,11 +273,13 @@ public class GameStart {
         System.out.println("Cliente Inserido com Sucesso: " + nomeCli + " | " + contatoCli + " | " + emailCli);
     }
 
-    // Menu Opção 6.2. Exibir Vagas Estacionamento
-    public static void exibirVagasEstacionamento() {
-        System.out.println("\n--- Vagas Disponíveis (Triangulares Múltiplos de 5) ---");
+    // Menu CLIENTE Opção 2. Exibir Vagas Estacionamento
+    public static void verificarVagasDisponiveis() {
+        System.out.println("\n<3 Vagas Disponíveis <3");
+
+        // Fórmula do número triangular
         for (int n = 1;; n++) {
-            int numVaga = (n * (n + 1)) / 2; // Fórmula do número triangular
+            int numVaga = (n * (n + 1)) / 2;
             if (numVaga > 121)
                 break;
             if (numVaga % 5 == 0) {
@@ -258,48 +288,45 @@ public class GameStart {
         }
     }
 
-    // Menu Opção 6.3. Exibir Jogos Sem Repetir
-    public static void exibirJogosSemRepetir(String[][] dados) {
-        System.out.println("\n--- Catálogo de Jogos ---");
-        //
+    // Menu CLIENTE Opção 3. Exibir Jogos Sem Repetir
+    public static void listarJogosUnicos(String[][] dados) {
+        System.out.println("\n<3 Catálogo de Jogos <3");
+
         for (int i = 0; i < dados.length; i++) {
             boolean duplicado = false;
+
             // Compara o jogo atual com todos os anteriores na matriz
             for (int j = 0; j < i; j++) {
-                if (dados[i][7].equals(dados[j][7])) {
+                if (dados[i][COL_JOGO].equals(dados[j][COL_JOGO])) {
                     duplicado = true;
                     break;
                 }
             }
             if (!duplicado) {
-                System.out.println("- " + dados[i][7]);
+                System.out.println("- " + dados[i][COL_JOGO]);
             }
         }
     }
 
-    // Menu Opção 6.4. Pesquisar por Editora
-    public static void pesquisarPorEditora(String[][] dados, Scanner teclado) {
+    // Menu CLIENTE Opção 4. Pesquisar por Editora
+    public static void listarJogosPorEditora(String[][] dados, Scanner teclado) {
         System.out.print("Editora a pesquisar: ");
         String editoraBusca = teclado.nextLine();
-        System.out.println("**** " + editoraBusca + " ****");
+        System.out.println("\n<3 " + editoraBusca + " <3");
 
-        // Vetor auxiliar para saber quais categorias já imprimimos (evita repetição do
-        // título da categoria)
         String catAnterior = "";
 
+        // Vetor auxiliar para saber quais categorias já imprimimos
         for (int i = 0; i < dados.length; i++) {
-            if (dados[i][5].equalsIgnoreCase(editoraBusca)) {
-                String catAtual = dados[i][6];
-                String jogo = dados[i][7];
+            if (dados[i][COL_EDITORA].equalsIgnoreCase(editoraBusca)) {
+                String catAtual = dados[i][COL_CATEGORIA];
+                String jogo = dados[i][COL_JOGO];
 
-                // Se a categoria desta linha for diferente da última que imprimimos, mostra o
-                // novo título
                 if (!catAtual.equalsIgnoreCase(catAnterior)) {
-                    System.out.println("\n" + "__" + catAtual + "__");
-                    catAnterior = catAtual; // Atualiza a categoria "marcada"
+                    System.out.println("<3" + catAtual + "<3");
+                    catAnterior = catAtual;
                 }
 
-                // Imprime o jogo logo abaixo da categoria correspondente
                 System.out.println(jogo);
             }
         }
